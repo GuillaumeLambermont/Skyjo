e1088c03f70823b6d87d1c7bf154dd91c4222388
Guillaume Lambermont <guillaume.lambermont@gmail.com>
2024-02-28 16:15:20 +0100

    Starting card stack

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,15 @@
 public class Card {
 
+    private int cardID;
     private int value;
+    private boolean isFaceUp;
+    private String location;
+
+    public Card(int cardID, int value, boolean isFaceUp, String location){
+        this.cardID = cardID;
+        this.value = value;
+        this.isFaceUp = isFaceUp;
+        this.location = location;
+    }
+
 }
